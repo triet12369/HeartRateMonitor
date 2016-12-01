30b0694b70fc78c825eb843fc935f0a1a1cc0e65
Kim Chi <kimchip26@gmail.com>
2016-12-01 10:35:11 +0700

    Graph:remove axis label,fix axis range;auto comment for Heartrate level

--- app/src/main/java/com/triet12369/heartratemonitor/MonitorFragment.java
+++ app/src/main/java/com/triet12369/heartratemonitor/MonitorFragment.java
@@ -1,5 +1,6 @@
 package com.triet12369.heartratemonitor;
 
+
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -7,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -15,7 +17,6 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 import java.util.Random;
 
 public class MonitorFragment extends Fragment {
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,71 +27,89 @@ public class MonitorFragment extends Fragment {
         GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
         mSeries1 = new LineGraphSeries<>(generateData());
         graph.addSeries(mSeries1);
+        graph.getGridLabelRenderer().setHighlightZeroLines(false); //remove highlight of zero lines
 
+        graph.getGridLabelRenderer().setHorizontalLabelsVisible(false);// remove horizontal x labels and line
+        graph.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(10);
+        graph.getViewport().setMinY(-2.0);
+        graph.getViewport().setMaxY(2.0);
+
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setXAxisBoundsManual(true);
+
+        TextView commentMessage = (TextView) rootView.findViewById(R.id.comment);
+        commentMessage.setText(Comment(heartVal));
+
+        TextView heartValue = (TextView) rootView.findViewById(R.id.HeartVal);
+        heartValue.setText(""+heartVal);
         return rootView;
     }
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle("Heart Rate Monitor");
-
-
     }
 
 
-        private final Handler mHandler = new Handler();
-        private Runnable mTimer1;
+    int heartVal = 120;
+    String heartComment=" ";
 
-        private LineGraphSeries<DataPoint> mSeries1;
+    private String Comment(int heartVal) {
 
-
-        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//
-//        }
-
-        public void onResume() {
-            super.onResume();
-            mTimer1 = new Runnable() {
-                @Override
-                public void run() {
-                    mSeries1.resetData(generateData());
-                    mHandler.postDelayed(this, 300);
-                }
-            };
-            mHandler.postDelayed(mTimer1, 300);
-
-
+        if ((heartVal >= 60) && (heartVal <= 100)) {
+            heartComment = "normal";
+        } else if (heartVal > 100) {
+            heartComment = "too high";
+        } else if (heartVal < 60) {
+            heartComment = "too low";
         }
+        String Message="Your heart rate is " + heartComment;
+        return Message;
+    }
 
-        @Override
-        public void onPause() {
-            mHandler.removeCallbacks(mTimer1);
+    private final Handler mHandler = new Handler();
+    private Runnable mTimer1;
+    private LineGraphSeries<DataPoint> mSeries1;
 
-            super.onPause();
-        }
-
-        private DataPoint[] generateData() {
-            int count = 30;
-            DataPoint[] values = new DataPoint[count];
-            for (int i=0; i<count; i++) {
-                double x = i;
-                double f = mRand.nextDouble()*0.15+0.3;
-                double y;
-                y = Math.sin(i*f+2) + mRand.nextDouble()*0.3;
-                DataPoint v = new DataPoint(x, y);
-                values[i] = v;
+    @Override
+    public void onResume() {
+        super.onResume();
+        mTimer1 = new Runnable() {
+            @Override
+            public void run() {
+                mSeries1.resetData(generateData());
+                mHandler.postDelayed(this, 300);
             }
-            return values;
-        }
+        };
+        mHandler.postDelayed(mTimer1, 300);
+    }
 
-        double mLastRandom = 2;
-        Random mRand = new Random();
-        private double getRandom() {
-            return mLastRandom += mRand.nextDouble()*0.5 - 0.25;
-        }
+//    @Override
+//    public void onPause() {
+//        mHandler.removeCallbacks(mTimer1);
+//
+//        super.onPause();
+//    }
 
+    private DataPoint[] generateData() {
+        int count = 10;
+        DataPoint[] values = new DataPoint[count];
+        for (int i=0; i<count; i++) {
+            double x = i;
+            double f = mRand.nextDouble()*0.15+0.3;
+            double y;
+            y = Math.sin(i*f+2) + mRand.nextDouble()*0.3;
+
+            DataPoint v = new DataPoint(x, y);
+            values[i] = v;
+        }
+        return values;
+    }
+    Random mRand = new Random();
 
 }
+
+
